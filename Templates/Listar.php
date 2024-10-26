685f2237d7b1801504005635ad7d7a88e4909be4
Alexis Salazar <alex01231@hotmail.com>
2024-10-26 14:23:05 -0500

    estilos

--- Templates/Listar.php
+++ Templates/Listar.php
@@ -3,8 +3,8 @@ require_once 'Entidades/Category.php';
 require_once 'Entidades/Product.php';
 ?>
 <section>
-    <div class="overflow-auto max-h-96	">
-        <div class="relative overflow-x-auto w-4/5 mt-4">
+    <div>
+        <div class="relative overflow-auto max-h-96 w-4/5 mt-4">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </header>
 
 <body ng-controller="myCtrl">
-    <div class="flex flex-col w-full ml-8">
+    <div class="flex flex-col ml-8">
         <section>
             <div class="mt-16" ng-if="!crear && !editar">
                 <button ng-click="crearProducto()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
