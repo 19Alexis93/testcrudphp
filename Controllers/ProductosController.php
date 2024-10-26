479f39931abce32b6089e04f7090de869d22d4f2
Alexis Salazar <alex01231@hotmail.com>
2024-10-26 14:34:21 -0500

    ajuste controlador

--- Controllers/ProductosController.php
+++ Controllers/ProductosController.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:8000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
